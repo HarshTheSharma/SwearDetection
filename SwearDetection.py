b84afe9465307b9764d8061d611f4fd03ca8e4a5
HarshTheSharma <71364679+HarshTheSharma@users.noreply.github.com>
2020-11-15 19:39:07 -0800

    Update SwearDetection.py

--- SwearDetection.py
+++ SwearDetection.py
@@ -3,14 +3,16 @@ import speech_recognition as sr
 r = sr.Recognizer()
 
 keyWord = '*'
+#The Google Api replaces swears with *'s
 
 with sr.Microphone() as source:
-    print('Please start speaking..\n')
-    while True: 
+    print('Please start speaking\n')
+    while True: cccccccccccccccccc
         audio = r.listen(source)
         try:
             text = r.recognize_google(audio)
             if keyWord.lower() in text.lower():
                 print('Keyword detected in the speech.')
+                swore = True
         except Exception as e:
             print('Please speak again.')
