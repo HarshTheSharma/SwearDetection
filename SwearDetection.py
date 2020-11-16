0f079fc437cc67ed8e107b9f7f56679df9a908cb
HarshTheSharma <71364679+HarshTheSharma@users.noreply.github.com>
2020-11-16 15:03:15 -0800

    Improved Comments

--- SwearDetection.py
+++ SwearDetection.py
@@ -1,9 +1,12 @@
+#imports
 import speech_recognition as sr
 
+#variables
 recognizer = sr.Recognizer()
 key_phrase = '*'
-# The Google API censors swear words with *s
+# The Google API censors swear words with "*" So this variable is essentially a list of every swear recognized by google
 
+#A loop comparing speech from the microphone to the variable above
 with sr.Microphone() as source:
     print('Please start speaking:\n')
     while True:
